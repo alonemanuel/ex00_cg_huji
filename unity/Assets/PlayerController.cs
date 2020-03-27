b46a95b966e1bfd621b1c45bc9bf169585103587
Alon Emanuel <alon@alonemanuel.com>
2020-03-27 18:48:16 +0300

    first commit really

--- unity/Assets/PlayerController.cs
+++ unity/Assets/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mail;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -11,12 +12,42 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        body = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        arrowKeysHandler();
         // Implement movement logic here
+    }
+
+    void arrowKeysHandler()
+    {
+        Vector3 direction = Vector3.zero;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direction = Vector3.forward;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction = Vector3.back;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = Vector3.right;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = Vector3.left;
+        }
+
+        if (direction != Vector3.zero)
+        {
+            body.AddForce(direction * movementForce * Time.deltaTime);
+        }
     }
 }
